784867f3feb49366b65ec88b7ae5bc47beae13c0
mton062 <wirtz.nicolas@gmail.com>
2013-11-27 22:39:32 +0100

    Modif Unity, suppression Winforms ajout PPT

--- tests_techniques/testKFET/Assets/GuiControllerMainScene.cs
+++ tests_techniques/testKFET/Assets/GuiControllerMainScene.cs
@@ -9,37 +9,56 @@ using System.ComponentModel;
 public class GuiControllerMainScene : MonoBehaviour 
 {
 	public Process process;
-	public Transform blabla;
-	public GameObject schtroumpf;
 	private bool etatToggle = false;
 	public GUISkin monskin;
+	private float xDep = 150f;
+	private float yDep = 150f;
 
 	void OnGUI()
-	{GUI.skin = monskin;
-		if(GUI.Button(new Rect(0,0,200,200),"GUI Test"))
+	{
+		GUI.skin = monskin;
+		if(Input.GetMouseButton(4))
 		{
-			process = new Process();
-			process.StartInfo.FileName = Application.dataPath + "/VS_solutionBidon.exe";
-
-
-			process.StartInfo.Arguments = "300;400";
-			process.Start();
+			MouseLook scriptMouse =  GameObject.FindGameObjectWithTag("Player").GetComponent<MouseLook>();
+			UnityEngine.Debug.Log(scriptMouse);
+			xDep = Screen.width/2-150;
+			yDep = Screen.height/2;
+			if(GUI.Button(new Rect(xDep,yDep,150,150),"Mode 1"))
+			{
+				
+			}
+			if(GUI.Button(new Rect(xDep + 150,yDep,150,150),""))
+			{
+				
+			}
+			if(GUI.Button(new Rect(xDep+300,yDep,150,150),"Mode 2"))
+			{
+				
+			}
+			if(GUI.Button(new Rect(xDep+150,yDep-150,150,150),"Mode 3"))
+			{
+				
+			}
+			if(GUI.Button(new Rect(xDep+150,yDep+150,150,150),"Mode 4"))
+			{
+				
+			}
+			//etatToggle = GUI.Toggle(new Rect(200,200,200,200),etatToggle,"yooo");
 		}
-		etatToggle = GUI.Toggle(new Rect(200,200,200,200),etatToggle,"yooo");
 
 
 	}
 	// Use this for initialization
 	void Start () 
 	{
-		schtroumpf = GameObject.FindGameObjectWithTag("Respawn");
+
 
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		schtroumpf.transform.Translate(new Vector3(0.1f,0,0));
+
 	}
 
 
