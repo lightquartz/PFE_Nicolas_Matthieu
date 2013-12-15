3b1a99bbe51a519036ed8f5a7c782bde1fd5ae41
mton062 <wirtz.nicolas@gmail.com>
2013-12-15 19:40:42 +0100

    Ajout raycast + curseur basique

---
+++ 3DCam_Unity/Assets/cursor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+public class cursor : MonoBehaviour 
+{
+	public Texture2D cursor_FPSView;
+	private Camera cam_fps;
+	Transform current_transform_obj = null;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Screen.showCursor = false;
+		Screen.lockCursor = true;
+		cam_fps = GameObject.FindGameObjectWithTag("MainCamera").camera;
+		if(cam_fps == null) UnityEngine.Debug.Log("Camera not found :/");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Ray cursor_ray = cam_fps.ScreenPointToRay(new Vector3(Screen.width/2,Screen.height/2));
+		RaycastHit hit;
+		if(Physics.Raycast(cursor_ray, out hit, Mathf.Infinity))
+		{
+			current_transform_obj = hit.transform;
+		}
+		else
+			current_transform_obj = null;
+	}
+
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect(Screen.width/2 -cursor_FPSView.width/2 , Screen.height/2 -cursor_FPSView.height/2,56,56),cursor_FPSView);
+		if(current_transform_obj!=null)
+			GUI.Label(new Rect(0,0,100,20),current_transform_obj.name);
+	}
+	void OnMouseDown()
+	{
+
+	}
+}
---
+++ tests_techniques/testKFET/Assets/Scripts/TV.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TV : Interactive
+{
+    private const string INTERACTIVE_MESSAGE = "Click To Change Channel";
+    public int currTVChannel = 1;
+    public Material[] tvChannelMaterials;
+
+    private MeshRenderer tvScreenMesh;
+    private GameObject tvDial; 
+
+    public void Awake()
+    {
+        tvScreenMesh = this.transform.FindChild("TVScreen").GetComponent<MeshRenderer>(); //get tv screen mesh
+        tvDial = this.transform.FindChild("RetroTV").FindChild("Dial").gameObject; //get tv dial
+        checkAvailableTVChannel();
+        updateTVChannel();
+    }
+
+    public override string GetInteractionMessage()
+    {
+        return INTERACTIVE_MESSAGE;
+    }
+
+    public override void Interact()
+    {
+        nextTVChannel();
+    }
+
+    //tests to make sure desired channel is not out of bounds
+    private void checkAvailableTVChannel()
+    {
+        if (currTVChannel<0)
+        {
+           currTVChannel=0;
+        }
+        else if (currTVChannel >= tvChannelMaterials.Length)
+        {
+            currTVChannel = tvChannelMaterials.Length - 1;
+        }
+    }
+
+    private void nextTVChannel()
+    {
+        currTVChannel++;
+        if (currTVChannel >= tvChannelMaterials.Length)
+        {
+            currTVChannel = 0;
+        }
+        changeChannel();
+    }
+
+    private void previousTVChannel()
+    {
+        currTVChannel--;
+        if (currTVChannel < 0)
+        {
+            currTVChannel = tvChannelMaterials.Length - 1;
+        }
+        changeChannel();
+    }
+
+    private void changeChannel()
+    {
+        updateTVChannel();
+        updateDialRotation();
+    }
+
+    private void updateTVChannel()
+    {
+        tvScreenMesh.material = tvChannelMaterials[currTVChannel];   
+    }
+
+    private void updateDialRotation()
+    {
+        tvDial.transform.Rotate(Vector3.forward,(360 / tvChannelMaterials.Length)); 
+    }
+}
